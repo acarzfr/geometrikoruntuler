a4a4d9b2b5ecd2b5f40bdd245d48a844bc8598ac
Çizgeler <45861141+acarzfr@users.noreply.github.com>
2024-11-21 11:06:56 +0300

    0

---
+++ story_content/user.js
@@ -0,0 +1,647 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6bscYMeNQWG":
+        Script1();
+        break;
+      case "6GxQXmTtmLV":
+        Script2();
+        break;
+      case "68flq4o5Dw3":
+        Script3();
+        break;
+      case "6oOpSuLwlvh":
+        Script4();
+        break;
+      case "64Nju1Uq2DJ":
+        Script5();
+        break;
+      case "5ldIaU8NuNO":
+        Script6();
+        break;
+      case "5kUt10goq1c":
+        Script7();
+        break;
+      case "5YluEFO9WZC":
+        Script8();
+        break;
+      case "5bQR53Vz6lu":
+        Script9();
+        break;
+      case "6D3dr6e5P0l":
+        Script10();
+        break;
+      case "6cZuxWu9iS0":
+        Script11();
+        break;
+      case "6GEf8IZEdxs":
+        Script12();
+        break;
+      case "5ahlyIdvOCj":
+        Script13();
+        break;
+      case "6QweGzGsPzZ":
+        Script14();
+        break;
+      case "5XK3SJKJFBo":
+        Script15();
+        break;
+      case "5ddSbEsfwnS":
+        Script16();
+        break;
+      case "60QA6lVbwhq":
+        Script17();
+        break;
+      case "68htkYYJRAA":
+        Script18();
+        break;
+      case "5fg6A640Zfl":
+        Script19();
+        break;
+      case "5WiIjdUfpWY":
+        Script20();
+        break;
+  }
+}
+
+function Script1()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure",seconds);
+if(count==0)
+{
+player.SetVar("durum","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script2()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure2",seconds);
+if(count==0)
+{
+player.SetVar("durum2","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script3()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure3",seconds);
+if(count==0)
+{
+player.SetVar("durum3","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script4()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure4",seconds);
+if(count==0)
+{
+player.SetVar("durum4","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script5()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure5",seconds);
+if(count==0)
+{
+player.SetVar("durum5","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script6()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure6",seconds);
+if(count==0)
+{
+player.SetVar("durum6","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script7()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure7",seconds);
+if(count==0)
+{
+player.SetVar("durum7","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script8()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure8",seconds);
+if(count==0)
+{
+player.SetVar("durum8","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script9()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure9",seconds);
+if(count==0)
+{
+player.SetVar("durum9","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script10()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure10",seconds);
+if(count==0)
+{
+player.SetVar("durum10","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script11()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure11",seconds);
+if(count==0)
+{
+player.SetVar("durum11","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script12()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure12",seconds);
+if(count==0)
+{
+player.SetVar("durum12","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script13()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure13",seconds);
+if(count==0)
+{
+player.SetVar("durum13","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script14()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure14",seconds);
+if(count==0)
+{
+player.SetVar("durum14","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script15()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure15",seconds);
+if(count==0)
+{
+player.SetVar("durum15","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script16()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure16",seconds);
+if(count==0)
+{
+player.SetVar("durum16","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script17()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure17",seconds);
+if(count==0)
+{
+player.SetVar("durum17","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script18()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure18",seconds);
+if(count==0)
+{
+player.SetVar("durum18","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script19()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure19",seconds);
+if(count==0)
+{
+player.SetVar("durum19","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
+function Script20()
+{
+  var seconds, timer;
+var counter = setInterval(timer, 1000);
+var player = GetPlayer();
+var count = 76;
+
+function zeros(i) 
+{
+return i;
+}
+function timer() {
+if(count >0)
+{
+    count = count -1;
+  }
+    seconds = zeros(count);
+player.SetVar("sure20",seconds);
+if(count==0)
+{
+player.SetVar("durum20","Süreniz Bitti");
+}
+}
+if (count < 0) {
+    clearInterval(counter);
+    return;
+}
+}
+
